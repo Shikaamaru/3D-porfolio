ce30cb16baa1728e3f38f242c6e874e62ce09d10
Arpit <arpityadava256@gmail.com>
2024-09-15 10:26:00 +0530

    second-2-commit

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -64,6 +64,8 @@ const ComputersCanvas = () => {
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
+           autoRotate
+           autoRotateSpeed={1} 
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
